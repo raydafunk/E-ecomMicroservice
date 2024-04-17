f6ec9e868f5d6025c5be7d4d1879d6a96336121b
Ray Brown-Amory <r.brown-amory@sky.com>
2024-04-17 19:09:41 +0100

    Implemented the Abstractions CORS

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProdutCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Catalog.API.Models;
+using CommonOperations.CQRS;
+
+namespace Catalog.API.Products.CreateProduct
+{
+
+    public record CreateProductCommand(string Name, List<string> Category,string Description,string ImageFile,decimal Price) 
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProdutCommandHandler : 
+        ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+   
+            // save the database 
+            // return the product result 
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
+
+            //Todo
+            // save the database 
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
