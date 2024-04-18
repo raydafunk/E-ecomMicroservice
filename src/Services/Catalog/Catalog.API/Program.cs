a71ebee2349d9d99e5ef7bac578a78ac5d42a200
Ray Brown-Amory <r.brown-amory@sky.com>
2024-04-18 12:38:27 +0100

    Completed the Registering  of  MediatR and Carter libraries into Asp.Net Dependency Injection Service

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,7 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to  the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 var app = builder.Build();
 
 // Configure the Http Request pipeline
+app.MapCarter();
+
 app.Run();
