5134a52bc76abcc7f03d34cfcbd099697355a82d
Ray Brown-Amory <r.brown-amory@sky.com>
2024-04-26 13:47:50 +0100

    create the vaildators

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductCommandHandler.cs
@@ -3,6 +3,14 @@ namespace Catalog.API.Products.DeleteProduct;
 public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
 
 public record DeleteProductResult(bool IsSuccess);
+
+public class DeleteProductCommandVaildator : AbstractValidator<DeleteProductCommand>
+{
+    public DeleteProductCommandVaildator()
+    {
+        RuleFor(x => x.Id).NotEmpty(). WithMessage(" The Product Id must be provide");
+    }
+}
 internal class DeleteProductCommandHandler(IDocumentSession session, ILogger<DeleteProductCommandHandler> logger)
     : ICommandHandler<DeleteProductCommand, DeleteProductResult>
 {
--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductCommandHandler.cs
@@ -1,10 +1,24 @@
 ﻿
-
 namespace Catalog.API.Products.UpdateProduct;
 public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
   : ICommand<UpdateProductResult>;
 
 public record UpdateProductResult(bool isSucesss);
+
+public class UpdateProductCommandHandlerVaildator : AbstractValidator<UpdateProductCommand>
+{
+    public UpdateProductCommandHandlerVaildator()
+    {
+        RuleFor(command => command.Id).NotEmpty().WithName("the Product  id is requered");
+
+        RuleFor(command => command.Name)
+               .NotEmpty().WithMessage("Name is required")
+               .Length(2, 150).WithMessage("Name must be and 150 characters");
+
+        RuleFor(command => command.Price)
+            .GreaterThan(0).WithMessage("The Price must be greater then 0");
+    }
+}
 internal class UpdateProductCommandHandler
     (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
     : ICommandHandler<UpdateProductCommand, UpdateProductResult>
