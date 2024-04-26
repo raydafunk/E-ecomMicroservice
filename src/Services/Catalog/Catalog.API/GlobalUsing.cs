ad761622b036475222d8e50e1e643f22830c5e3e
Ray Brown-Amory <r.brown-amory@sky.com>
2024-04-26 09:27:17 +0100

    created Cross-cutting Concerns: Validate with AbstractValidator using Fluent Validation started the validation

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -5,5 +5,5 @@ global using Catalog.API.Models;
 global using CommonOperations.CQRS;
 global using Marten;
 global using Catalog.API.Excceptions;
-
+global using FluentValidation;
 
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProdutCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProdutCommandHandler.cs
@@ -5,11 +5,31 @@ namespace Catalog.API.Products.CreateProduct
     public record CreateProductCommand(string Name, List<string> Category,string Description,string ImageFile,decimal Price) 
         : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProdutCommandHandler (IDocumentSession session)
+
+    public class CreateProductCommandVaildator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandVaildator()
+        {
+                RuleFor(x => x.Name).NotEmpty().WithMessage("Name is Required");
+                RuleFor(x => x.Category).NotEmpty().WithMessage("Category is Required");
+                RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is Required");
+                RuleFor(x => x.Price).GreaterThan(0).WithMessage("ImageFile is Required");
+        }
+    }
+    internal class CreateProdutCommandHandler (
+          IDocumentSession session, IValidator<CreateProductCommand> validator)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
+
+            var result = await validator.ValidateAsync(command, cancellationToken);
+            var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+            if (errors.Any())
+            {
+                throw new ValidationException(errors.FirstOrDefault());
+            }
+
             var product = new Product
             {
                 Name = command.Name,
