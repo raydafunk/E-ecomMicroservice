916e856680a80d462d433ac3ebfedb84a47c116c
Ray Brown-Amory <r.brown-amory@sky.com>
2024-05-03 13:52:53 +0100

    Developed  Basket.API Microservices Creating Domain Entities for the Basket

---
+++ src/Services/Basket/Basket.API/Models/Class.cs
@@ -0,0 +1,22 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        public string Username { get; set; } = default!;
+
+        public List<ShoppingCartItem> items { get; set; } = new();
+
+        public decimal TotalPrice => items.Sum(x=> x.Price * x.Quantity);
+
+        public ShoppingCart(string userName)
+        {
+                Username = userName;
+        }
+
+        //Requried the mapping
+        public ShoppingCart()
+        {
+                
+        }
+    }
+}
