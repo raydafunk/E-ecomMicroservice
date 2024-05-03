d868e56b0d92464752e767c5d62184d26c8ea070
Ray Brown-Amory <r.brown-amory@sky.com>
2024-05-03 18:55:34 +0100

    Develop StoreBasket Feature Handler class with CQRS and MediatR

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasektCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasektCommand>
+{
+    public StoreBasketCommandValidator()
+    {
+        RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+        RuleFor(x => x.Cart.Username).NotEmpty().WithMessage("UserName is required");
+    }
+}
+public class StoreBasketCommandHandler :
+    ICommandHandler<StoreBasektCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasektCommand command, CancellationToken cancellationToken)
+    {
+        ShoppingCart cart = command.Cart;
+
+        //TODO: store basket in database(use Marten update- if exist = update, if not)
+        //TODO: update cache
+
+        return new StoreBasketResult("swn");
+    }
+}
--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Basket.API.Basket.StoreBasket
-{
-    public class StoreBasketHandler
-    {
-    }
-}
