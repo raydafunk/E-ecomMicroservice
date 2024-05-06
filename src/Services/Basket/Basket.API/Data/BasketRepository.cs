20e32ff9db72dece619742011d8d6ca11e2ee5fd
Ray Brown-Amory <r.brown-amory@sky.com>
2024-05-06 09:37:15 +0100

    Develop BasketRepository Class using Marten Library placeholder for I Basket Interface for abstraction

---
+++ src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Basket.API.Data
+{
+    public class BasketRepository : IBasketRepository
+    {
+     
+        public Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
