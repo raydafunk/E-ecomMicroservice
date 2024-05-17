2bfbbf76f7180995486c52d5ca43685faf127526
Ray Brown-Amory <r.brown-amory@sky.com>
2024-05-17 10:56:07 +0100

    Created SQLite Database using Entity Framework Core Tools in Discount service

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,15 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public class DiscountContext : DbContext
+    {
+        public DbSet<Coupon> Coupones { get; set; } = default!;
+
+        public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,12 +1,15 @@
 
 
+using Discount.Grpc.Data;
 using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
-
+builder.Services.AddDbContext<DiscountContext>(opts =>
+opts.UseSqlite(builder.Configuration.GetConnectionString("Database")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
