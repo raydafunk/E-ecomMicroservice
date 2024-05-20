15c8d9d574f384d067e6557c5a69329a923f869f
Ray Brown-Amory <r.brown-amory@sky.com>
2024-05-20 11:20:45 +0100

    creating the sql database for the browser

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -13,7 +13,7 @@ opts.UseSqlite(builder.Configuration.GetConnectionString("Database")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseMigration();
+ app.UseMigration();
 app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
