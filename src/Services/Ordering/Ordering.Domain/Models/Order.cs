415a1975d7d1d5f28bc0a5264818ac8ca6df5713
Ray Brown-Amory <r.brown-amory@sky.com>
2024-06-02 09:04:17 +0100

    Mapping DDD Objects to EF Entities - Develop EF Core Entity Configurations

--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -3,13 +3,9 @@
 public class Order : Aggregate<OrderId>
 {
     private readonly List<OrderItem> _orderItems = new();
-
     public IReadOnlyList<OrderItem> OrderItems  => _orderItems.AsReadOnly();
-
     public CustomerId CustomerId { get; private set; } = default!;
-
     public OrderName OrderName { get; private set; } = default!;
-
     public Address ShippingAddress { get; private set; } = default!;
     public Address BillingAddress { get; private set; } = default!;
     public Payment Payment { get; private set; } = default!;
---
+++ src/Services/Ordering/Ordering.Infrastruture/Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+
+namespace Ordering.Infrastruture.Data.Configurations
+{
+    internal class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
