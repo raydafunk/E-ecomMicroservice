1a698949206bfe4bef3a9a5a3f618ddc68112790
Ray Brown-Amory <r.brown-amory@sky.com>
2024-06-12 10:33:30 +0100

    Run Order Microservices to Migrate and Seed OrderDb in SQL Server using EF Core

--- src/Services/Ordering/Ordering.Infrastruture/Data/Extensions/DatabaseExtentions.cs
+++ src/Services/Ordering/Ordering.Infrastruture/Data/Extensions/DatabaseExtentions.cs
@@ -19,8 +19,8 @@ namespace Ordering.Infrastruture.Data.Extensions
         private static async Task SeedAsync(ApplicationDbContext context)
         {
             await SeedCustomerAsync(context);
-            //await SeedProductAsync(context);
-            //await SeedOrderandItems(context);
+            await SeedProductAsync(context);
+            await SeedOrderWithItemsAsync(context);
         }
 
         private static async Task SeedCustomerAsync(ApplicationDbContext context)
@@ -31,5 +31,22 @@ namespace Ordering.Infrastruture.Data.Extensions
                 await context.SaveChangesAsync();
             }
         }
+        private static async Task SeedProductAsync(ApplicationDbContext context)
+        {
+            if (!await context.Products.AnyAsync())
+            {
+                await context.Products.AddRangeAsync(InitialData.Products);
+                await context.SaveChangesAsync();   
+            }
+        }
+        private static async Task SeedOrderWithItemsAsync(ApplicationDbContext context)
+        {
+            if (!await context.Orders.AnyAsync())
+            {
+                await context.Orders.AddRangeAsync(InitialData.OrdersWithItems);
+                await context.SaveChangesAsync();
+            }
+        }
+
     }
 }
