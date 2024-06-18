429ac4681909db39d58809b22b9d791d69285058
Ray Brown-Amory <r.brown-amory@sky.com>
2024-06-18 16:13:37 +0100

    Developed the Create Orders command and handlers

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,20 @@
+﻿using CommonOperations.CQRS;
+using FluentValidation;
+using Ordering.Application.Dtos;
+
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public record CreateOrderCommand(OrderDto Order) : ICommand<CreateOrderResult>;
+
+public record CreateOrderResult(Guid Id);
+
+public class CreateOrderValidator: AbstractValidator<CreateOrderCommand>
+{
+	public CreateOrderValidator()
+	{
+		RuleFor(x => x.Order.OrderName).NotEmpty().WithMessage("Name is required");
+		RuleFor(x => x.Order.CustomerId).NotEmpty().WithMessage("CustomerId is required");
+		RuleFor(x => x.Order.OrderItems).NotEmpty().WithMessage("OrderItems is required");
+	}
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,12 @@
+﻿
+using CommonOperations.CQRS;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public class CreateOrderHandler : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+{
+    public Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
