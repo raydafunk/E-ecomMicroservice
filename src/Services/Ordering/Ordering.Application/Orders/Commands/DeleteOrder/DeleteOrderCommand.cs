21d8a8a1357ef0dcd16724d2060c6e1083ee44b8
Ray Brown-Amory <r.brown-amory@sky.com>
2024-06-24 22:36:30 +0100

    Developed the DeleteOrderHandle and command

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Orders.Commands.DeleteOrder;
+
+public record DeleteOrderCommand(Guid OrderId) : ICommand<DeleteOrderResult>;   
+
+public record DeleteOrderResult(bool IsSuccess);
+
+public class DeleteOrderValidator : AbstractValidator<DeleteOrderCommand>
+{
+    public DeleteOrderValidator()
+    {
+        RuleFor(r => r.OrderId).NotEmpty().WithMessage("the OrderId is Requried");
+    }
+}
