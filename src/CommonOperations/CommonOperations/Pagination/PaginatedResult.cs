d9f21f5f44ced9a9b05c1428686a1dafc71065cc
Ray Brown-Amory <r.brown-amory@sky.com>
2024-06-29 15:11:00 +0100

    Develop the Pagaination class  for the query GetOrders using CQRS and meditor

---
+++ src/CommonOperations/CommonOperations/Pagination/PaginatedResult.cs
@@ -0,0 +1,11 @@
+﻿namespace CommonOperations.Pagination
+{
+    public  class PaginatedResult<TEnity>(int pageIndex, int pageSize, long count, IEnumerable<TEnity> data ) 
+        where TEnity : class
+    {
+        public int PageIndex { get; } = pageIndex;
+        public int PageSize { get; } = pageSize;
+        public long Count { get; } = count;
+        public IEnumerable<TEnity> Data { get; } = data;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,3 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrders;
+
+ public record GetOrdersQuery(PaginationRequest )
