be609d26ccbbdcc5eb618338d13a4553335dfe32
Ray Brown-Amory <r.brown-amory@sky.com>
2024-07-04 18:20:44 +0100

    fixed the nuget packget problem and install Cater

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,18 +1,20 @@
-﻿namespace Ordering.API;
+﻿using Carter;
+
+namespace Ordering.API;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddApiServices(this IServiceCollection services)
     {
-        //services.AddCarter();
+        services.AddCarter();
 
         return services;
     }
 
-    public static WebApplication UseApiServices( this WebApplication application)
+    public static WebApplication UseApiServices( this WebApplication app)
     {
-        // app.MapCarter();
+        app.MapCarter();
 
-        return application;
+        return app;
     }
 }
