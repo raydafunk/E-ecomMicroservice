647d21e754472a58230828da7f6a3c46f129e6f8
Ray Brown-Amory <r.brown-amory@sky.com>
2024-07-08 12:29:22 +0100

    Developing the Create order API

--- src/Services/Ordering/Ordering.API/EndPoints/CreateOrder.cs
+++ src/Services/Ordering/Ordering.API/EndPoints/CreateOrder.cs
@@ -1,11 +1,35 @@
-﻿namespace Ordering.API.EndPoints
+﻿using Ordering.Application.Orders.Commands.CreateOrder;
+namespace Ordering.API.EndPoints
 {
 
-    //-Accepts a CreateOrderRequest object
+
     //-Mapps the  request to a CreateOrderCommand
     //Uses MeidatR to send the command to the Corresponding order
     //- Returns a response with the created order's ID 
-    public class CreateOrder
+
+    //-Accepts a CreateOrderRequest object
+    public record CreateOrderRequest(OrderDto Order);   
+    public record CreateOrderResponse(Guid Id);
+  
+    public class CreateOrder : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.Map("/orders", async (CreateOrderRequest request, ISender sender) =>
+            {
+                var comand = request.Adapt<CreateOrderCommand>();
+                
+                var results = await sender.Send(comand);
+
+                var response = results.Adapt<CreateOrderResponse>();
+
+                return Results.Created($"/orders/{response.Id}", response);
+            })
+                .WithName("CreateOrder")
+                .Produces<CreateOrderResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Create Order")
+                .WithDescription("Create Order");
+        }
     }
 }
