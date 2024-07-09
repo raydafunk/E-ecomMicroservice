0f83a245ca4a5701e87b73b203134c2078d511bf
Ray Brown-Amory <r.brown-amory@sky.com>
2024-07-09 16:25:46 +0100

    Delevped  and impeleted the Developing GetOrdersByCustomer Endpoint

---
+++ src/Services/Ordering/Ordering.API/EndPoints/GetOrdersByCustomer.cs
@@ -0,0 +1,27 @@
+﻿
+using Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+namespace Ordering.API.EndPoints
+{
+
+    public record GetOrdersByCustomerResponse(IEnumerable<OrderDto> Orders);
+    public class GetOrdersByCustomer : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders/customer/{customerId}", async (Guid customerId, ISender sender) =>
+            {
+                var result = await sender.Send(new GetOrdersByCustomerQuery(customerId));
+
+                var response = result.Adapt<GetOrdersByCustomerResponse>();
+
+                return Results.Ok(response);
+            }).WithName("GetOrdersByCustomer")
+                .Produces<CreateOrderResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithSummary("Get Orders ByCustomer")
+                .WithDescription("Get Orders by Customer"); ;
+        }
+    }
+}
