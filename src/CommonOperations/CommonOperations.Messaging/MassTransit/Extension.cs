3959233c313371b1e72ed3e434084c26d9134c4e
Ray Brown-Amory <r.brown-amory@sky.com>
2024-08-06 00:53:05 +0100

    Developed MassTransit Extention Methods to Register RabbitMQ

--- src/CommonOperations/CommonOperations.Messaging/MassTransit/Extension.cs
+++ src/CommonOperations/CommonOperations.Messaging/MassTransit/Extension.cs
@@ -1,13 +1,35 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace CommonOperations.Messaging.MassTransit
 {
     public static class Extensions
     {
-        public static IServiceCollection AddMessageBroker(this IServiceCollection services, Assembly? assembly = null)
+        public static IServiceCollection AddMessageBroker(this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
         {
             //Implemted RabbitMQ MassTransit configuration
+            services.AddMassTransit(config =>
+            {
+                config.SetKebabCaseEndpointNameFormatter();
+
+                if (assembly != null)
+                {
+                    config.AddConsumers(assembly);
+                }
+                config.UsingRabbitMq((context, configurator) =>
+                {
+                    configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                    {
+                        host.Username(configuration["MessageBroker:UserName"]!);
+                        host.Password(configuration["MessageBroker:Password"]!);
+
+                    });
+                    configurator.ConfigureEndpoints(context);
+                });
+            });
+
             return services;
         }
     }
