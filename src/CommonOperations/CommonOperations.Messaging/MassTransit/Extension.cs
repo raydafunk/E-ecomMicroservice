465db26dab535a4c9b47b1e1ed1f32fb56a7c4ef
Ray Brown-Amory <r.brown-amory@sky.com>
2024-08-06 00:34:39 +0100

    Develop MassTransit Extention Methods to Register RabbitMQ

---
+++ src/CommonOperations/CommonOperations.Messaging/MassTransit/Extension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CommonOperations.Messaging.MassTransit
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddMessageBroker(this IServiceCollection services, Assembly? assembly = null)
+        {
+            //Implemted RabbitMQ MassTransit configuration
+            return services;
+        }
+    }
+}
