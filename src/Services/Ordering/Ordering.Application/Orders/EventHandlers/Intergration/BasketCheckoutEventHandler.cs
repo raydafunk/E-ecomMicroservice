7040ed401eab770106b981dea489f2c2c38722b6
Ray Brown-Amory <r.brown-amory@sky.com>
2024-08-08 17:45:04 +0100

    Created Domain created events to Intergation events

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Intergration/BasketCheckoutEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using CommonOperations.Messaging.Events;
+using MassTransit;
+
+namespace Ordering.Application.Orders.EventHandlers.Intergration
+{
+    public class BasketCheckoutEventHandler : IConsumer<BasketCheckoutEvent>
+    {
+        public Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
